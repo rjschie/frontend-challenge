2954011c35f96982bdee2d30d3af22f5df91fe3e
Ryan Schie <rjschie@gmail.com>
2015-09-07 10:15:59 -0700

    make list sortable

--- src/js/lists.js
+++ src/js/lists.js
@@ -4,5 +4,19 @@ $(function() {
 	$('[data-toggle=popover]').popover({
 		placement: 'bottom'
 	});
-	
+
+	/** Make List Sortable */
+	$('.js-list-sortable').sortable({
+		items: "> .list__item",
+		forcePlaceholderSize: true,
+		placeholder: "list__item list__item--highlight",
+		tolerance: "pointer",
+		start: function(e, ui) {
+			ui.item.addClass('list__item--grabbing');
+		},
+		stop: function(e, ui) {
+			ui.item.removeClass('list__item--grabbing');
+		}
+	});
+
 });
