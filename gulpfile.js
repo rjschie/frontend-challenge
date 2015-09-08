ddc29afdcb1644ea4e83eb5c88d4270c97adfdde
Ryan Schie <rjschie@gmail.com>
2015-09-07 19:01:38 -0700

    converted tabs to spaces

--- gulpfile.js
+++ gulpfile.js
@@ -13,17 +13,17 @@ var htmlInjector = require('bs-html-injector');
  * Usefuls
  */
 var config = {
-	path : {
-		root	: 'src',
-		css		: 'src/css',
-		js		: 'src/js',
-		assets: 'src/assets'
-	}
+  path : {
+    root	: 'src',
+    css		: 'src/css',
+    js		: 'src/js',
+    assets: 'src/assets'
+  }
 };
 
 function errorOutput(error) {
-	plugins.util.log("Error ( " + error.plugin + " ): " + error.message);
-	this.emit('end');
+  plugins.util.log("Error ( " + error.plugin + " ): " + error.message);
+  this.emit('end');
 }
 
 
@@ -31,75 +31,75 @@ function errorOutput(error) {
  * Development Tasks
  */
 gulp.task('css', function() {
-	return gulp.src(config.path.css + '/combined.scss')
-		.pipe(plugins.sourcemaps.init())
-			.pipe(plugins.sass({ outputStyle: 'compressed' })
-				.on('error', errorOutput))
-			.pipe(plugins.autoprefixer('last 2 version', 'safari 5', 'ie 8',
-				'ie 9', 'opera 12.1')
-				.on('error', errorOutput))
-		.pipe(plugins.sourcemaps.write())
-		.pipe(gulp.dest(config.path.css)
-			.on('error', errorOutput))
-		.pipe(bs.stream());
+  return gulp.src(config.path.css + '/combined.scss')
+    .pipe(plugins.sourcemaps.init())
+      .pipe(plugins.sass({ outputStyle: 'compressed' })
+        .on('error', errorOutput))
+      .pipe(plugins.autoprefixer('last 2 version', 'safari 5', 'ie 8',
+        'ie 9', 'opera 12.1')
+        .on('error', errorOutput))
+    .pipe(plugins.sourcemaps.write())
+    .pipe(gulp.dest(config.path.css)
+      .on('error', errorOutput))
+    .pipe(bs.stream());
 });
 
 
 
 gulp.task('js', function() {
-	return gulp.src(config.path.js + '/all.js')
-		.pipe(plugins.include())
-		.pipe(plugins.sourcemaps.init())
-			.pipe(plugins.concat('app.js')
-				.on('error', errorOutput))
-			.pipe(plugins.uglify()
-				.on('error', errorOutput))
-		.pipe(plugins.sourcemaps.write())
-		.pipe(gulp.dest(config.path.js)
-			.on('error', errorOutput));
+  return gulp.src(config.path.js + '/all.js')
+    .pipe(plugins.include())
+    .pipe(plugins.sourcemaps.init())
+      .pipe(plugins.concat('app.js')
+        .on('error', errorOutput))
+      .pipe(plugins.uglify()
+        .on('error', errorOutput))
+    .pipe(plugins.sourcemaps.write())
+    .pipe(gulp.dest(config.path.js)
+      .on('error', errorOutput));
 });
 
 
 
 gulp.task('sprites', function() {
-	return gulp.src(config.path.assets + '/svg/separate/*.svg')
-		.pipe(plugins.svgSprite(
-		{
-			shape: {
-				dimension: {
-					maxWidth: 100,
-					maxHeight: 100
-				}
-			},
-			svg: {
-				transform: [
-					function(svg) {
-						svg = svg.replace(/<g\s.+?>|<\/g>/g, '');
-						svg = svg.replace(/fill=".+?"/g, '');
-						svg = svg.replace(/stroke=".+?"/g, '');
-						return svg;
-					}
-				]
-			},
-			mode: {
-				symbol: {
-					dest: '.',
-					sprite: 'sprite.svg',
-					inline: true
-				}
-			}
-		}).on('error', errorOutput))
-		.pipe(gulp.dest(config.path.assets + '/svg'));
+  return gulp.src(config.path.assets + '/svg/separate/*.svg')
+    .pipe(plugins.svgSprite(
+    {
+      shape: {
+        dimension: {
+          maxWidth: 100,
+          maxHeight: 100
+        }
+      },
+      svg: {
+        transform: [
+          function(svg) {
+            svg = svg.replace(/<g\s.+?>|<\/g>/g, '');
+            svg = svg.replace(/fill=".+?"/g, '');
+            svg = svg.replace(/stroke=".+?"/g, '');
+            return svg;
+          }
+        ]
+      },
+      mode: {
+        symbol: {
+          dest: '.',
+          sprite: 'sprite.svg',
+          inline: true
+        }
+      }
+    }).on('error', errorOutput))
+    .pipe(gulp.dest(config.path.assets + '/svg'));
 });
 
 
 
 gulp.task('images', function() {
-	return gulp.src(config.path.assets + '/imgs/**/*')
-		.pipe(plugins.imagemin({
-			optimizationLevel: 7
-		}))
-		.pipe(gulp.dest(config.path.assets + '/imgs'));
+  return gulp.src(config.path.assets + '/imgs/**/*')
+    .pipe(plugins.imagemin({
+      optimizationLevel: 7
+    }))
+    .pipe(gulp.dest(config.path.assets + '/imgs'));
 });
 
 
@@ -108,24 +108,24 @@ gulp.task('images', function() {
  * Watching
  */
 gulp.task('watch', function() {
-	bs.use(htmlInjector, {
-		files: config.path.root + '/**/*.html'
-	});
-	bs.init({
-		server: {
-			baseDir: './src'
-		},
-		open: false
-	});
+  bs.use(htmlInjector, {
+    files: config.path.root + '/**/*.html'
+  });
+  bs.init({
+    server: {
+      baseDir: './src'
+    },
+    open: false
+  });
 
-	gulp.watch(config.path.root + '/**/*.html')
-		.on('change', htmlInjector)
-		.on('error',errorOutput);
-	gulp.watch(config.path.root + '/**/*.js', ['js'])
-		.on('change', bs.reload)
-		.on('error',errorOutput);
-	gulp.watch(config.path.root + '/**/*.scss', ['css'])
-		.on('error',errorOutput);
+  gulp.watch(config.path.root + '/**/*.html')
+    .on('change', htmlInjector)
+    .on('error',errorOutput);
+  gulp.watch(config.path.root + '/**/*.js', ['js'])
+    .on('change', bs.reload)
+    .on('error',errorOutput);
+  gulp.watch(config.path.root + '/**/*.scss', ['css'])
+    .on('error',errorOutput);
 });
 
 
--- src/js/lists-events.js
+++ src/js/lists-events.js
@@ -3,82 +3,82 @@
  * Slide new list into view
  */
 function advanceMenu(from, toward, options) {
-	var options = options || {};
-	options.offset = options.offset || '120%';
-	options.direction = options.direction || 'left';
-	options.speed = options.speed || 500;
-	options.hideClass = options.hideClass || 'list__group--hidden';
+  var options = options || {};
+  options.offset = options.offset || '120%';
+  options.direction = options.direction || 'left';
+  options.speed = options.speed || 500;
+  options.hideClass = options.hideClass || 'list__group--hidden';
 
-	toward.removeClass(options.hideClass);
-	toward.css({
-		position: 'absolute',
-		top: 0,
-		left: options.offset
-	});
-	from.css('height', toward.height());
-	toward.animate({
-		left: 0
-	}, options.speed);
-	from.hide('slide', {direction: options.direction}, options.speed, function() {
-		from.addClass(options.hideClass);
-		from.removeAttr('style');
-		toward.removeAttr('style');
-	});
+  toward.removeClass(options.hideClass);
+  toward.css({
+    position: 'absolute',
+    top: 0,
+    left: options.offset
+  });
+  from.css('height', toward.height());
+  toward.animate({
+    left: 0
+  }, options.speed);
+  from.hide('slide', {direction: options.direction}, options.speed, function() {
+    from.addClass(options.hideClass);
+    from.removeAttr('style');
+    toward.removeAttr('style');
+  });
 }
 
 
 
 $(function() {
 
-	/** Toggle popovers */
-	$('[data-toggle=popover]').popover({
-		placement: 'bottom',
-		container: '.list'
-	});
+  /** Toggle popovers */
+  $('[data-toggle=popover]').popover({
+    placement: 'bottom',
+    container: '.list'
+  });
 
 
-	/** Make List Sortable */
-	$('.js-list-sortable').sortable({
-		items: "> .list__item",
-		forcePlaceholderSize: true,
-		placeholder: "list__item list__item--placeholder",
-		tolerance: "pointer",
-		distance: 10,
-		cancel: '.js-list-item-back,.list__action-wrap',
-		start: function(e, ui) {
-			ui.item.addClass('list__item--grabbing');
-		},
-		stop: function(e, ui) {
-			ui.item.removeClass('list__item--grabbing');
-		}
-	});
+  /** Make List Sortable */
+  $('.js-list-sortable').sortable({
+    items: "> .list__item",
+    forcePlaceholderSize: true,
+    placeholder: "list__item list__item--placeholder",
+    tolerance: "pointer",
+    distance: 10,
+    cancel: '.js-list-item-back,.list__action-wrap',
+    start: function(e, ui) {
+      ui.item.addClass('list__item--grabbing');
+    },
+    stop: function(e, ui) {
+      ui.item.removeClass('list__item--grabbing');
+    }
+  });
 
 
-	/** List menu'ing */
-	$('.js-list-item-menu').on('click', function(e) {
-		var self = $(this);
-		var parent = self.parent();
-		var target = $('#' + self.data('target'));
+  /** List menu'ing */
+  $('.js-list-item-menu').on('click', function(e) {
+    var self = $(this);
+    var parent = self.parent();
+    var target = $('#' + self.data('target'));
 
-		if( typeof(e.target.className) == 'string'
-			&& e.target.className.indexOf('list__action-button') > -1 ) {
+    if( typeof(e.target.className) == 'string'
+      && e.target.className.indexOf('list__action-button') > -1 ) {
 
-			return false;
-		}
+      return false;
+    }
 
-		if( target.find('.js-list-item-back').length == 0 ) {
-			var backButton = $(target.data('back-template')) || $('<li></li>');
-			target.prepend(backButton);
+    if( target.find('.js-list-item-back').length == 0 ) {
+      var backButton = $(target.data('back-template')) || $('<li></li>');
+      target.prepend(backButton);
 
-			backButton.on('click', function() {
-				advanceMenu(target, parent, {
-					offset: '-150%',
-					direction: 'right'
-				});
-			});
-		}
+      backButton.on('click', function() {
+        advanceMenu(target, parent, {
+          offset: '-150%',
+          direction: 'right'
+        });
+      });
+    }
 
-		advanceMenu(parent, target);
-	});
+    advanceMenu(parent, target);
+  });
 
 });
